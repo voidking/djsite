12541652089b890596bc8fa39e8a73b7715bc024
voidking <voidking@qq.com>
2017-11-27 18:12:45 +0800

    first template

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
 def hello(request):
     return HttpResponse('helloworld')
