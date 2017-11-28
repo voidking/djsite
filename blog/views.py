154f65dd7f0ac62dc0771842faa85d7a8bf2dc19
voidking <voidking@qq.com>
2017-11-28 18:39:07 +0800

    完成Model格式转JSON格式

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from django.forms.models import model_to_dict
+from django.core import serializers
 from . import models
 
 # Create your views here.
@@ -22,16 +24,20 @@ def list(request):
 
 
 def add(request):
-    # title = request.POST.get('title', 'defaultTitle')
-    # content = request.POST.get('content', 'defaultContent')
-
     title = request.GET.get('title', 'defaultTitle')
     content = request.GET.get('content', 'defaultContent')
 
     article = models.Article.objects.create(title=title, content=content)
-
-    result = {'code': 0, 'ext': 'success', 'article_id': article.id}
-    return HttpResponse(json.dumps(result))
+    article = model_to_dict(article)
+    articles = models.Article.objects.all()
+    json_data = serializers.serialize("json", articles)
+    dict_data = json.loads(json_data)
+    result = {
+        'code': 0,
+        'ext': 'success',
+        'article': article,
+        'articles': dict_data}
+    return HttpResponse(json.dumps(result, ensure_ascii=False))
 
 
 def edit(request):
@@ -45,10 +51,11 @@ def edit(request):
     article.save()
 
     result = {'code': 0, 'ext': 'success', 'article_id': article.id}
-    return HttpResponse(json.dumps(result))
+    return HttpResponse(json.dumps(result, ensure_ascii=False))
+
 
 def delete(request):
     article_id = request.GET.get('id', 0)
     models.Article.objects.get(pk=article_id).delete()
     result = {'code': 0, 'ext': 'success'}
-    return HttpResponse(json.dumps(result))
+    return HttpResponse(json.dumps(result, ensure_ascii=False))
