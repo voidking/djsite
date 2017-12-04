15752792ba1559463662fe52acf7fd0679f9a3c9
voidking <voidking@qq.com>
2017-12-04 14:22:09 +0800

    更换mysql

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,11 @@ from . import models
 # Register your models here.
 
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # 显示在admin控制台中的列名
+    list_display = ('title', 'content', 'pub_time')
+    # 时间过滤器
+    list_filter = ('pub_time',)
+
+
+admin.site.register(models.Article, ArticleAdmin)
--- djsite/settings.py
+++ djsite/settings.py
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'djsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'djsite',
+        'USER': 'root',
+        'PASSWORD': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
